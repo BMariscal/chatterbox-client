f13febb08a9f1e68da49a8d10141650503625700
Briceida <BriceidaMariscal@gmail.com>
2017-10-22 10:32:26 -0700

    filters by room and changes your room name based on the room you select

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -4,6 +4,7 @@
 var app = {};
 app.currentDataLength = 0;
 app.data;
+app.roomname;
 app.lastMessageId;
 app.rooms = [];
 app.send = function(message) {
@@ -15,7 +16,7 @@ app.send = function(message) {
     contentType: 'application/json',
     success: function (data) {
       console.log('chatterbox: Message sent');
-     
+
     },
     error: function (data) {
     // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -25,7 +26,6 @@ app.send = function(message) {
 };
 
 app.init = function() {
-  
   app.fetch('http://parse.sfm6.hackreactor.com/chatterbox/classes/messages');
 
   setInterval(function() {
@@ -45,8 +45,8 @@ app.fetch = function(url) {
       app.data = data.results;
       var mostRecentMessage = app.data[0];
       if (mostRecentMessage.objectId !== app.lastMessageId) {
-              
-          app.renderMessage(app.data);
+
+          app.renderMessage(app.data, app.roomname);
 
       }
       app.lastMessageId = app.data[0].objectId;
@@ -63,9 +63,9 @@ app.clearMessages = function() {
 app.renderMessage = function(dataM) {
   app.clearMessages();
   // dataM = Array.from(dataM);
- 
+
   dataM.forEach(function(dataMessage) {
-    dataMessage.username = _.escape(dataMessage.username);   
+    dataMessage.username = _.escape(dataMessage.username);
     dataMessage.text = _.escape(dataMessage.text);
     dataMessage.roomname = _.escape(dataMessage.roomname);
     if (!app.rooms.includes(dataMessage.roomname)) {
@@ -73,8 +73,8 @@ app.renderMessage = function(dataM) {
       $('select').append(`<option value=${dataMessage.roomname}>${dataMessage.roomname}</option>`);
     }
 
-    var room = $(`<div class="room ${dataMessage.roomname}">Room: ${dataMessage.roomname}</div>`);
-    var message = $(`<div class="message ${dataMessage.username}"></div>`);
+    var room = $(`<div class="room ${dataMessage.roomname}"> Room: ${dataMessage.roomname}</div>`);
+    var message = $(`<div class="message ${dataMessage.username} ${dataMessage.roomname}"></div>`);
     var text = $(`<div class="text">${dataMessage.text}</div>`);
     var username = $(`<button class="username" onclick="app.handleUsernameClick(this)">${dataMessage.username}</button>`);
     room.appendTo(message);
@@ -82,6 +82,7 @@ app.renderMessage = function(dataM) {
     username.appendTo(message);
     message.appendTo($('#chats'));
   });
+  app.changeRoom(app.roomname);
 };
 
 app.renderRoom = function(array) {
@@ -91,19 +92,25 @@ app.renderRoom = function(array) {
 
 };
 //LOOK AT ME!
-// app.changeRoom = function(roomname) {
-//   console.log("inside changeRoom", roomname);
-//   $('.message').toggleClass('.hideRoom');
-//   $('.' + roomname).toggleClass('.showRoom');
-
-// };
+app.changeRoom = function(roomname) {
+  app.roomname = roomname;
+  console.log("inside changeRoom", roomname);
+  if(!roomname || roomname === "allrooms"){
+    console.log("all rooms");
+    $('.message').removeClass('hideRoom');
+    $('.message').addClass('showRoom');
+  }else{
+  $('.message').addClass('hideRoom');
+  $('.message' + '.'+ roomname).removeClass('hideRoom');
+  }
+};
 
 app.handleUsernameClick = function(username) {
   var theUser = $(username).html().split(' ')[0];
   $('.' + theUser).toggleClass('friend');
-    
-};  
-  
+
+};
+
 
 
 app.handleSubmit = function(event, message) {
--- client/scripts/interactions.js
+++ client/scripts/interactions.js
@@ -1,25 +1,14 @@
  $(document).ready(function() {
    console.log( 'ready!');
-   $('.username').on('click', function() {
-     console.log('I am running, i am button!');
-   });   
-//LOOK AT ME!
-   // $('.roomSubmit').on('click', function() {
-   //   console.log("inside ROOM SUBMIT");
-   //   var roomname = window.location.search.split('&')[0].split('=')[1];
-   //   app.changeRoom(roomname);
-   // });
 
    $('.clearBtn').on('click', function() {
      app.clearMessages();
    });
-
    $('#send.submit').on('click', function(event) {
-     var username = window.location.search.split('&')[1].split('=')[1];
-     var roomname = window.location.search.split('&')[0].split('=')[1];
+     var username = window.location.search.split('=')[1];
+     roomname = $("[name='roomSelect']").val();
 
      var value = $('#message').val();
-     console.log(this.username);
      var message = {username: username, text: value, roomname: roomname};
      app.handleSubmit(event, message);
      $('textarea').val('');
